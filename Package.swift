e225dab3b7e5d4b1ff60777a88843072c2f7ea81
jinho-wannagohome <jinho.jang@wesang.com>
2022-08-15 16:44:47 +0900

    customize

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/zhangao0086/DKCamera", .branch("develop")),
         .package(url: "https://github.com/zrn-ns/DKPhotoGallery", .branch("feature/support_swiftpm")),
-        .package(url: "https://github.com/TimOliver/TOCropViewController", from: "2.6.0"),
+        .package(url: "https://github.com/jinho-wannagohome/TOCropViewController.git", .branch("spm")),
     ],
     targets: [
         .target(
